2ab51905767b1eecb367dd39bc39f474e0290c6b
Matt Kramer <matthew123003@gmail.com>
2024-03-02 17:11:48 -0500

    First test method

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,20 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+    @Test;
+    public void compare(){
+        Basics basicTests = new Basics();
+        int value = basicTests.compare();
+        Assert.assertEquals();
+
+
+    }
 }
